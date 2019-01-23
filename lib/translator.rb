f564e66d76cabd0dfd1a1b83cd19c444890528a8
camille feghali <camfeg@gmail.com>
2019-01-23 16:12:08 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,16 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+def load_library (file_path)
+  emoticons = YAML.load(File.read("#{file_path}"))
+  # binding.pry
+end
+
+def get_japanese_emoticon (path, eng)
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
 
 def get_english_meaning
   # code goes here
